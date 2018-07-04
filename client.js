2235155d53241ff638de7161c1da042a61136578
Ethan Wolz <ethan@creadiv.io>
2018-07-04 13:04:30 -0600

    messing with client

--- client.js
+++ client.js
@@ -1,9 +1,35 @@
 const socket = require('socket.io-client')('http://localhost:1337')
 const {blue, green, red} = require('chalk')
+const readline = require('readline')
+
 
 socket.on('connect', () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.setPrompt("> ")
+  rl.prompt()
+
+  rl.on('line', (line) => {
+    if (line == '/clear') {
+      process.stdout.write('\033c')
+    }
+    line    = line.toString().trim()
+
+    if (!line.match(/^\//)) {
+      return socket.emit('send', line)
+    }
+
+    command = line.split(' ')[0].slice(1)
+    args    = line.split(' ').slice(1)
+
+    socket.emit(command, args.join(' '))
+    rl.prompt()
+  })
+
   socket.emit('nick', 'Wolzey')
-  socket.emit('join', 'backroom')
 
   socket.on('user join', ({user, room}) => {
     console.log(blue(user), green('just joined room'), red.bold(room))
@@ -14,14 +40,10 @@ socket.on('connect', () => {
   })
 
   socket.on('help', (commands) => {
-    console.log(commands + '\n')
+    process.stdout(commands + '\n')
   })
 
-  socket.emit('help')
+  // stdin.addListener('data', (data) => {
 
-  socket.emit('send', 'Hey there!')
-  socket.emit('send', 'How are you?')
-  socket.emit('send', 'I\'m good!')
-
-  socket.emit('exit')
+  // })
 })
